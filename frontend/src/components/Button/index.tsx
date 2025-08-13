c5ed32fb20ed5fb2f50235c2d3fd8cada7732950
GooseOb <gooose.ob@gmail.com>
2025-08-13 15:49:39 +0200

    remove children props

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,16 +1,10 @@
 import styles from './Button.module.css';
 
 export const Button = ({
-  children,
   variant = 'primary',
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  children?: React.ReactNode;
   variant?: 'primary' | 'round' | 'ghost';
 }) => {
-  return (
-    <button className={styles[variant]} {...props}>
-      {children}
-    </button>
-  );
+  return <button className={styles[variant]} {...props}></button>;
 };
--- frontend/src/components/InputLabel/index.tsx
+++ frontend/src/components/InputLabel/index.tsx
@@ -1,9 +1,7 @@
 import styles from './InputLabel.module.css';
 
 export const InputLabel = (
-  props: React.LabelHTMLAttributes<HTMLLabelElement> & {
-    children?: React.ReactNode;
-  }
+  props: React.LabelHTMLAttributes<HTMLLabelElement>
 ) => {
   return <label className={styles.label} {...props}></label>;
 };
